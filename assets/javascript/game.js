94e2c43274891ebd07865dcabc955fcfc3982c1e
Stan Mozolevskiy <stanmozolevskiy@gmail.com>
2018-10-12 21:54:58 -0400

    half way js is done, working game. Needs to represent score,wins and loses counter and css

---
+++ assets/javascript/game.js
@@ -0,0 +1,57 @@
+window.onload = function() {
+
+    var randomNumberTarget = Math.floor((Math.random() * 30) + 30);
+
+    console.log("random number is" + " " + randomNumberTarget);
+
+    $("#number-to-guess").append(randomNumberTarget);
+    var score = 0;
+    var scoreNumberForCristals = [1, 10, 3, 7]
+
+    function shuffle(arrey) {
+
+        var currentIndex = scoreNumberForCristals.length,
+            temporaryValue, randomIndex;
+
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+            // And swap it with the current element.
+            temporaryValue = scoreNumberForCristals[currentIndex];
+            scoreNumberForCristals[currentIndex] = scoreNumberForCristals[randomIndex];
+            scoreNumberForCristals[randomIndex] = temporaryValue;
+        }
+        return scoreNumberForCristals;
+
+    }
+
+    arr = shuffle(scoreNumberForCristals);
+
+    for (var i = 0; i < arr.length; i++) {
+        console.log(arr[i]);
+
+        var imageCrystal = $("<img>");
+        imageCrystal.addClass("crystal-image");
+        imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
+        imageCrystal.attr("data-crystalvalue", arr[i]);
+        $("#crystals").append(imageCrystal);
+
+    }
+    $(".crystal-image").click(function() {
+        var crystalValue = ($(this).attr("data-crystalvalue"));
+        crystalValue = parseInt(crystalValue); //returns integer from string
+        score += crystalValue; // adds values to score each time cristal is clicked
+
+        alert("New score: " + score);
+
+        if (score === randomNumberTarget) {
+            alert("You win!");
+        } else if (score >= randomNumberTarget) {
+            alert("You lose!!");
+        }
+
+
+    })
+}
