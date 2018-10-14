59ebb9ddb8c071ad1f4a2cb36066672accf2fe58
Stan Mozolevskiy <stanmozolevskiy@gmail.com>
2018-10-13 20:10:55 -0400

    almost ready but can make some changes later next week

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,75 +1,88 @@
+var score = 0;
+var scoreNumberForCristals = [1, 10, 3, 7];
+var cristalData = 0;
+var randomNumberTarget, imageCrystal;
+var wins = 0;
+var loses = 0;
+
+
+
 window.onload = function() {
-
-    var randomNumberTarget = Math.floor((Math.random() * 30) + 30);
-    var score = 0;
-
-    console.log("random number is" + " " + randomNumberTarget);
-    $("#number-to-guess").append(randomNumberTarget);
-    $("#score-text").html("your score is" + " " + score); //sets the score for the begining of the game
+    var audioCrystal = $("#audio");
+    var audioWin = $("#audio-win");
+    var audioLose = $("#audio-lose");
 
 
-    var scoreNumberForCristals = [1, 10, 3, 7]
 
-    function shuffle(arrey) {
 
-        var currentIndex = scoreNumberForCristals.length,
-            temporaryValue, randomIndex;
+    function newGame() {
 
-        // While there remain elements to shuffle...
-        while (0 !== currentIndex) {
-            // Pick a remaining element...
-            randomIndex = Math.floor(Math.random() * currentIndex);
-            currentIndex -= 1;
-            // And swap it with the current element.
-            temporaryValue = scoreNumberForCristals[currentIndex];
-            scoreNumberForCristals[currentIndex] = scoreNumberForCristals[randomIndex];
-            scoreNumberForCristals[randomIndex] = temporaryValue;
+        randomNumberTarget = Math.floor((Math.random() * 30) + 30);
+
+        console.log("random number is" + " " + randomNumberTarget);
+        $("#number-to-guess").html(" " + randomNumberTarget);
+        $("#score-text").html("Your score is:" + " " + score); //sets the score for the begining of the game
+        $("#wins-text").html("Wins:" + " " + wins); //sets the wins for the begining of the game
+        $("#loses-text").html("Loses:" + " " + loses); //sets the loses for the begining of the game
+
+
+
+        function shuffle(arrey) {
+
+            var currentIndex = scoreNumberForCristals.length,
+                temporaryValue, randomIndex;
+
+            // While there remain elements to shuffle...
+            while (0 !== currentIndex) {
+                // Pick a remaining element...
+                randomIndex = Math.floor(Math.random() * currentIndex);
+                currentIndex -= 1;
+                // And swap it with the current element.
+                temporaryValue = scoreNumberForCristals[currentIndex];
+                scoreNumberForCristals[currentIndex] = scoreNumberForCristals[randomIndex];
+                scoreNumberForCristals[randomIndex] = temporaryValue;
+            }
+            return scoreNumberForCristals;
+
         }
-        return scoreNumberForCristals;
 
+
+        var arr = shuffle(scoreNumberForCristals);
+
+        //forlupe create an imege
+
+        for (var i = 0; i < arr.length; i++) {
+            console.log(arr[i]);
+            imageCrystal = $("#image" + i);
+            imageCrystal.attr("data-score", arr[i]);
+            $("#crystals").append(imageCrystal);
+
+        }
     }
-
-
-    arr = shuffle(scoreNumberForCristals);
-
-    for (var i = 0; i < arr.length; i++) {
-        console.log(arr[i]);
-
-        var imageCrystal = $("<img>");
-        imageCrystal.addClass("crystal-image");
-        imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
-        imageCrystal.attr("data-crystalvalue", arr[i]);
-        $("#crystals").append(imageCrystal);
-
-    }
-    var clickFunction = $(".crystal-image");
-
-
-    clickFunction.click(function count(num) {
-        var crystalValue = ($(this).attr("data-crystalvalue"));
-        crystalValue = parseInt(crystalValue); //returns integer from string
-        score += crystalValue; // adds values to score each time cristal is clicked
-
-
-        // alert("New score: " + score);
+    $(".crystal-image").click(function count(event) {
+        audioCrystal.get(0).play();
+        //assign data- score to varcrystalValue
+        var crystalValue = ($(this).attr("data-score"));
+        //returns integer from string
+        crystalValue = parseInt(crystalValue);
+        // adds values to score each time cristal is clicked
+        score += crystalValue;
+        // updates the score
+        $("#score-text").html("your score is" + " " + score)
 
         if (score === randomNumberTarget) {
-            alert("Win!!");
-            count();
             score = 0;
-
-
-
+            wins++;
+            document.getElementById("audio-win").play();
+            newGame();
         } else
         if (score >= randomNumberTarget) {
-            alert("You lose!!");
 
-
+            score = 0;
+            loses++
+            document.getElementById("audio-lose").play();
+            newGame();
         }
-
-
-        $("#score-text").html("your score is" + " " + score) // updates the score
-
     })
-
+    newGame();
 }
